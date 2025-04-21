dcac7f82932d6ee7cddfb301136f83201007f848
parkchangjun <ory01@hs.ac.kr>
2025-04-21 17:24:57 +0900

    JDBC upload

--- 06_PARKCHANGJUN/chap02-statements-lecture-source/src/main/java/com/ohgiraffers/section01/statement/Application1.java
+++ 06_PARKCHANGJUN/chap02-statements-lecture-source/src/main/java/com/ohgiraffers/section01/statement/Application1.java
@@ -1,9 +1,11 @@
 package com.ohgiraffers.section01.statement;
 
+import java.awt.*;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import static com.ohgiraffers.common.JDBCTemplate.close;
 import static com.ohgiraffers.common.JDBCTemplate.getConnection;
@@ -12,28 +14,45 @@ public class Application1 {
 
     public static void main(String[] args) {
 
-        /* 트랜잭션 처리를 위한 DMBS 연동용 Connection 객체 생성 */
+        /* 트랜잭션 처리를 위한 DBMS 연동용 Connection 객체 생성 */
         Connection con = getConnection();
         System.out.println("con = " + con);
 
-        Statement stmt = null; // 쿼리를 운반하고 결과를 반환
-        ResultSet rset = null; // 조회할 예정(DML 작업이면 ResultSet 대신 int로 처리)
+        Statement stmt = null;          // 쿼리를 운반하고 결과를 반환
+        ResultSet rset = null;          // 조회할 예정(DML 작업이면 ResultSet 대신 int로 처리)
 
         try {
-            stmt = con.createStatement(); // console 열은거랑 똑같음
+            stmt = con.createStatement();
             rset = stmt.executeQuery("SELECT * FROM tbl_menu");
 
-            /* while문 안이 rset은 한 행을 의미 */
-            while (rset.next()) {
-                System.out.print(rset.getString("menu_name") + " ");
-                System.out.print(rset.getInt("menu_price") + "\n");
+            /* while문 안의 rset은 한 행을 의미*/
+//            while(rset.next()) {
+//                System.out.print(rset.getString("menu_name") + "  ");
+//                System.out.print(rset.getInt("menu_price") + "\n");
+//            }
+
+            ArrayList<MenuDTO> list = null;
+            while(rset.next()) {
+                list = new ArrayList<>();
+                list.add(new MenuDTO(rset.getString("menu_name"), rset.getInt("menu_price")));
+            }
+
+            for(MenuDTO dto : list) {
+                System.out.println(dto);
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         } finally {
             close(rset);
             close(stmt);
-            close(con); // 선언된거 역순으로 하면 된다.
+            close(con);
         }
     }
 }
+
+
+
+
+
+
+
--- 06_PARKCHANGJUN/chap02-statements-lecture-source/src/main/java/com/ohgiraffers/section03/sqlinjection/Application1.java
+++ 06_PARKCHANGJUN/chap02-statements-lecture-source/src/main/java/com/ohgiraffers/section03/sqlinjection/Application1.java
@@ -1,37 +1,35 @@
-package com.ohgiraffers.section01.statement;
-
+package com.ohgiraffers.section03.sqlinjection;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Scanner;
 
 import static com.ohgiraffers.common.JDBCTemplate.close;
 import static com.ohgiraffers.common.JDBCTemplate.getConnection;
 
-public class Application2 {
-    /* 메뉴코드를 입력받아서 하나의 메뉴를 조회하는 기능 */
+public class Application1 {
+    private static String empId = "200";
+    //    private static String empName = "홍길동";
+    private static String empName = "' OR 1=1 AND EMP_ID = '200";
+
     public static void main(String[] args) {
 
         Connection con = getConnection();
         Statement stmt = null;
         ResultSet rset = null;
 
+        String query = "SELECT * FROM employee WHERE emp_id = '" + empId + "' AND EMP_NAME = '" + empName + "'";
+        System.out.println("query = " + query);
+
         try {
             stmt = con.createStatement();
-
-            Scanner sc = new Scanner(System.in);
-            System.out.print("메뉴코드를 입력하세요 : ");
-            int menuCode = sc.nextInt();
-
-            String query = "SELECT * FROM tbl_menu WHERE menu_code = " + menuCode;
-//            String query = "SELECT * FROM tbl_menu WHERE menu_name = '" + menuName + "'";
-            System.out.println("query = " + query);
-
             rset = stmt.executeQuery(query);
+
             if(rset.next()) {
-                System.out.println(rset.getString("menu_name") + "  " + rset.getInt("menu_price"));
+                System.out.println(rset.getString("EMP_NAME") + "님 환영합니다.");
+            }else {
+                System.out.println("회원 정보가 없습니다.");
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -39,6 +37,8 @@ public class Application2 {
             close(rset);
             close(stmt);
             close(con);
+
         }
+
     }
 }
